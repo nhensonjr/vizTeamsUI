6b83db2fc7e591511dadfae9cd72f5ab1bdf251d
NHENSON <norman.henson@vizientinc.com>
2019-09-17 16:16:17 -0500

    hopefully fix heroku deployment

---
+++ dist/vizTeamsUI/assets/mock-teams.ts
@@ -0,0 +1,275 @@
+import { Team } from '../app/Models/team.model';
+
+export const MOCKTEAMS: Team[] = [
+  {
+    id: 1,
+    name: 'P2P',
+    description: '',
+    members: [
+      {
+        team: 1, id: 15, firstName: 'Dylan', lastName: 'Geile',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/15/200'
+      },
+      {
+        team: 1, id: 16, firstName: 'Blake', lastName: 'Rhodes',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/16/200'
+      },
+      {
+        team: 1, id: 17, firstName: 'Roger', lastName: 'Beasley',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/17/200'
+      },
+      {
+        team: 1, id: 18, firstName: 'Joey', lastName: 'Froemling',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/18/200'
+      },
+      {
+        team: 1, id: 19, firstName: 'Brandon', lastName: 'Rachelski',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/19/200'
+      },
+      {
+        team: 1, id: 20, firstName: 'Bryce', lastName: 'Bigham',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/20/200'
+      },
+      {
+        team: 1, id: 21, firstName: 'Jonathon', lastName: 'Bryant',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/21/200'
+      },
+      {
+        team: 1, id: 22, firstName: 'Zack', lastName: 'Amis',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/22/200'
+      },
+      {
+        team: 1, id: 23, firstName: 'Ben', lastName: 'Asselmeier',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/23/200'
+      },
+      {
+        team: 1, id: 24, firstName: 'Travis', lastName: 'Blessing',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/24/200'
+      },
+      {
+        team: 1, id: 25, firstName: 'Dylan', lastName: 'Stark',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/25/200'
+      },
+      {
+        team: 1, id: 26, firstName: 'Nathan', lastName: 'Waller',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/26/200'
+      },
+      {
+        team: 1, id: 27, firstName: 'Norman', lastName: 'Henson',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/27/200'
+      },
+      {
+        team: 1, id: 28, firstName: 'Anupa', lastName: 'Vemula',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/28/200'
+      },
+      {
+        team: 1, id: 29, firstName: 'Tammy', lastName: 'Sotak',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/29/200'
+      },
+      {
+        team: 1, id: 30, firstName: 'Lauren', lastName: 'Jones',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/30/200'
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Catalog',
+    description: '',
+    members: [
+      {
+        team: 2, id: 31, firstName: 'Zaelin', lastName: 'Swann',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/31/200'
+      },
+      {
+        team: 2, id: 32, firstName: 'William', lastName: 'Ralston',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/32/200'
+      },
+      {
+        team: 2, id: 33, firstName: 'Lora', lastName: 'Hish',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/33/200'
+      },
+      {
+        team: 2, id: 34, firstName: 'Greg', lastName: 'Trent',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/34/200'
+      },
+      {
+        team: 2, id: 35, firstName: 'Mark', lastName: 'Miller',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/35/200'
+      },
+    ]
+  },
+  {
+    id: 3,
+    name: 'Cornerstone',
+    description: '',
+    members: [
+      {
+        team: 3, id: 36, firstName: 'Ryan', lastName: 'Everett',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/36/200'
+      },
+      {
+        team: 3, id: 37, firstName: 'Sara', lastName: 'Hovis',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/37/200'
+      },
+      {
+        team: 3, id: 38, firstName: 'Graham', lastName: 'Walker',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/38/200'
+      },
+      {
+        team: 3, id: 39, firstName: 'Benjamin', lastName: 'Froemling',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/39/200'
+      },
+      {
+        team: 3, id: 40, firstName: 'Benjamin', lastName: 'Martin',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/40/200'
+      },
+      {
+        team: 3, id: 41, firstName: 'Ryan', lastName: 'Brock',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/41/200'
+      },
+      {
+        team: 3, id: 42, firstName: 'Brandon', lastName: 'Clark',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/42/200'
+      },
+      {
+        team: 3, id: 43, firstName: 'William', lastName: 'Atkins',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/43/200'
+      },
+      {
+        team: 3, id: 44, firstName: 'Sidhant', lastName: 'Amatya',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/44/200'
+      },
+    ]
+  },
+  {
+    id: 4,
+    name: 'Data Crispr',
+    description: '',
+    members: [
+      {
+        team: 4, id: 45, firstName: 'Mark', lastName: 'Burks',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/45/200'
+      },
+      {
+        team: 4, id: 46, firstName: 'Alex', lastName: 'Dunn',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/46/200'
+      },
+      {
+        team: 4, id: 47, firstName: 'Hunter', lastName: 'Hartline',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/47/200'
+      },
+      {
+        team: 4, id: 48, firstName: 'Nathaniel', lastName: 'Berry',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/48/200'
+      },
+      {
+        team: 4, id: 49, firstName: 'Stephen', lastName: 'Sladek',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/49/200'
+      },
+      {
+        team: 4, id: 50, firstName: 'Jacob', lastName: 'Legrand',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/50/200'
+      },
+      {
+        team: 4, id: 51, firstName: 'Zachary', lastName: 'Weston',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/51/200'
+      },
+    ]
+  },
+  {
+    id: 5,
+    name: 'CLO',
+    description: '',
+    members: [
+      {
+        team: 5, id: 52, firstName: 'Matthew', lastName: 'Skaggs',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/52/200'
+      },
+      {
+        team: 5, id: 53, firstName: 'Andrew', lastName: 'Palmer',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/53/200'
+      },
+      {
+        team: 5, id: 54, firstName: 'Christopher', lastName: 'Dunning',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/54/200'
+      },
+      {
+        team: 5, id: 55, firstName: 'Brian', lastName: 'Giunta',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/55/200'
+      },
+      {
+        team: 5, id: 56, firstName: 'Fatah', lastName: 'Cherfaoui',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/56/200'
+      },
+      {
+        team: 5, id: 57, firstName: 'Manjil', lastName: 'Ghimire',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/57/200'
+      },
+    ]
+  },
+  {
+    id: 6,
+    name: 'SSO',
+    description: '',
+    members: [
+      {
+        team: 6, id: 58, firstName: 'Gary', lastName: 'Rusher',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/58/200'
+      },
+      {
+        team: 6, id: 59, firstName: 'Kirk', lastName: 'Tolleshaug',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/59/200'
+      },
+      {
+        team: 6, id: 60, firstName: 'Ethan', lastName: 'Gibbons',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/60/200'
+      },
+      {
+        team: 6, id: 61, firstName: 'Matthew', lastName: 'Cook',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/61/200'
+      },
+      {
+        team: 6, id: 62, firstName: 'Tonya', lastName: 'Wilson',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/62/200'
+      },
+      {
+        team: 6, id: 63, firstName: 'Melissa', lastName: 'Stovall',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/63/200'
+      },
+    ]
+  },
+  {
+    id: 7,
+    name: 'VizGan',
+    description: '',
+    members: [
+      {
+        team: 7, id: 64, firstName: 'Cameron', lastName: 'Hutchison',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/64/200'
+      },
+      {
+        team: 7, id: 65, firstName: 'Krysten', lastName: 'Byers',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/65/200'
+      },
+      {
+        team: 7, id: 66, firstName: 'Pola', lastName: 'Mikhael',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/66/200'
+      },
+      {
+        team: 7, id: 67, firstName: 'Senthil', lastName: 'Sundaramoorthy',
+        title: 'Software Engineer', pathToPhoto: 'https://picsum.photos/id/67/200'
+      },
+      {
+        team: 7, id: 68, firstName: 'Hiral', lastName: 'Patel',
+        title: 'Quality Engineer', pathToPhoto: 'https://picsum.photos/id/68/200'
+      },
+    ]
+  },
+  {
+    id: 7,
+    name: 'EmptyTeam',
+    description: '',
+    members: []
+  },
+];
---
+++ server.js
@@ -0,0 +1,27 @@
+//Install express server
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// Serve only the static files form the dist directory
+
+
+  app.use(express.static('./dist/vizTeamsUI'));
+
+
+  app.get('/*', function (req, res) {
+
+
+    res.sendFile(path.join(__dirname, '/dist/vizTeamsUI/index.html'));
+
+
+  });
+
+
+// Start the app by listening on the default Heroku port
+
+
+app.listen(process.env.PORT || 8080);
