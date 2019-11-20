ad947a7a1269ced9be909d6c344798e745ff774e
Norman Henson Jr <nhensonjr@gmail.com>
2019-11-19 20:43:22 -0600

    prevent empty teams/members

--- src/app/components/add-dialog/add-dialog.component.ts
+++ src/app/components/add-dialog/add-dialog.component.ts
@@ -1,13 +1,13 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef, MatPaginator, PageEvent } from '@angular/material';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Member } from '../../models/member.model';
-import { AddMemberDialogData } from '../../interfaces/add-member-dialog-data.interface';
-import { Team } from '../../models/team.model';
-import { MemberService } from '../../services/member/member.service';
-import { PhotoService } from '../../services/photo/photo.service';
-import { Picture } from '../../models/picture.model';
-import { TeamService } from '../../services/team/team.service';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef, MatPaginator, PageEvent} from '@angular/material';
+import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {Member} from '../../models/member.model';
+import {AddMemberDialogData} from '../../interfaces/add-member-dialog-data.interface';
+import {Team} from '../../models/team.model';
+import {MemberService} from '../../services/member/member.service';
+import {PhotoService} from '../../services/photo/photo.service';
+import {Picture} from '../../models/picture.model';
+import {TeamService} from '../../services/team/team.service';
 
 @Component({
   selector: 'app-add-dialog',
@@ -15,10 +15,10 @@ import { TeamService } from '../../services/team/team.service';
       <div *ngIf="data.team; else teamDialog">
           <form class="example-container" [formGroup]="memberForm">
               <div class="add-dialog__image-list">
-              <span class="add-dialog__image-container" *ngFor="let pic of pictureURLs.slice(firstImage, lastImage)">
-                  <img [ngClass]="pic === selectedPic ? 'add-dialog__selected-image' : 'add-dialog__image'"
-                       (click)="setSelectedPic(pic)" src="{{pic.url}}" alt="">
-              </span>
+                <span class="add-dialog__image-container" *ngFor="let pic of pictureURLs.slice(firstImage, lastImage)">
+                    <img [ngClass]="pic === selectedPic ? 'add-dialog__selected-image' : 'add-dialog__image'"
+                         (click)="setSelectedPic(pic)" src="{{pic.url}}" alt="">
+                </span>
               </div>
               <mat-paginator #matPaginator
                              [length]="pictureURLs.length"
@@ -28,27 +28,31 @@ import { TeamService } from '../../services/team/team.service';
 
               <div class="add-dialog__info-container">
                   <mat-form-field>
-                      <input matInput placeholder="First Name" formControlName="firstName">
+                      <input matInput placeholder="First Name" formControlName="firstName" required>
+                      <mat-error>First name required</mat-error>
                   </mat-form-field>
 
                   <mat-form-field>
-                      <input matInput placeholder="Last Name" formControlName="lastName">
+                      <input matInput placeholder="Last Name" formControlName="lastName" required>
+                      <mat-error>Last name required</mat-error>
                   </mat-form-field>
               </div>
 
               <div class="add-dialog__info-container">
                   <mat-form-field>
-                      <mat-select placeholder="Title" formControlName="title">
+                      <mat-select placeholder="Title" formControlName="title" required>
                           <mat-option value="Software Engineer">Software Engineer</mat-option>
                           <mat-option value="Quality Engineer">Quality Engineer</mat-option>
                       </mat-select>
+                      <mat-error>Job title required</mat-error>
                   </mat-form-field>
 
                   <mat-form-field>
-                      <mat-select placeholder="Team" formControlName="team">
+                      <mat-select placeholder="Team" formControlName="team" required>
                           <mat-option [value]="this.data.team.id">{{data.team.name}}</mat-option>
                           <mat-option *ngFor="let team of data.allTeams" value="{{team.id}}">{{team.name}}</mat-option>
                       </mat-select>
+                      <mat-error>Team required</mat-error>
                   </mat-form-field>
               </div>
 
@@ -61,7 +65,8 @@ import { TeamService } from '../../services/team/team.service';
       <ng-template #teamDialog>
           <form class="example-container" [formGroup]="teamForm">
               <mat-form-field>
-                  <input matInput placeholder="Team Name" formControlName="name">
+                  <input matInput placeholder="Team Name" formControlName="name" required>
+                  <mat-error>Team name required</mat-error>
               </mat-form-field>
 
               <mat-form-field>
@@ -82,6 +87,7 @@ export class AddDialogComponent implements OnInit {
   firstImage = 0;
   lastImage = 5;
   pictureURLs: Picture[] = [];
+  avatarUrl = '../../assets/avatar.png';
 
   teamForm = new FormGroup({
     name: new FormControl(''),
@@ -89,7 +95,7 @@ export class AddDialogComponent implements OnInit {
   });
 
   memberForm = new FormGroup({
-    pathToPhoto: new FormControl(''),
+    pathToPhoto: new FormControl(this.avatarUrl),
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     title: new FormControl(''),
@@ -109,8 +115,6 @@ export class AddDialogComponent implements OnInit {
     if (this.data.team) {
       this.memberForm.get('team').setValue(this.data.team.id);
     }
-    this.memberForm.get('team').setValue('some value');
-
     this.photoService.getPhotos().subscribe((imageList: Picture[]) => {
       imageList.forEach((image, index) => {
         this.pictureURLs.push(new Picture(image.id));
@@ -119,26 +123,35 @@ export class AddDialogComponent implements OnInit {
   }
 
   onMemberSubmit(form: FormGroup): void {
-    const newMember = new Member(
-      form.get('team').value,
-      form.get('firstName').value,
-      form.get('lastName').value,
-      form.get('title').value,
-      form.get('pathToPhoto').value
-    );
+    if (this.memberFormHasErrors(form)) {
+      this.setMemberFormErrors(form);
+    } else {
+      const newMember = new Member(
+        form.get('team').value,
+        form.get('firstName').value,
+        form.get('lastName').value,
+        form.get('title').value,
+        form.get('pathToPhoto').value
+      );
 
-    this.memberService.createMember(newMember).subscribe();
-    this.dialogRef.close();
+      this.memberService.createMember(newMember).subscribe();
+      this.dialogRef.close();
+    }
   }
 
   onTeamSubmit(form: FormGroup): void {
-    const newTeam = new Team(
-      form.get('name').value,
-      form.get('description').value
-    );
+    if (form.get('name').value.trim() !== '') {
+      const newTeam = new Team(
+        form.get('name').value,
+        form.get('description').value
+      );
 
-    this.teamService.createTeam(newTeam).subscribe();
-    this.dialogRef.close();
+      this.teamService.createTeam(newTeam).subscribe();
+      this.dialogRef.close();
+    } else {
+      this.teamForm.get('name').setValue('');
+      this.teamForm.get('name').setErrors({incorrect: true});
+    }
   }
 
   onCancel(): void {
@@ -154,5 +167,40 @@ export class AddDialogComponent implements OnInit {
   setSelectedPic(pic: Picture) {
     this.selectedPic = pic;
     this.memberForm.get('pathToPhoto').setValue(pic.url);
+  }
+
+  setMemberFormErrors(form: FormGroup): void {
+    if (form.get('team').value === '') {
+      form.get('team').setValue('');
+      form.get('team').setErrors({incorrect: true});
+    }
+    if (form.get('firstName').value.trim() === '') {
+      form.get('firstName').setValue('');
+      form.get('firstName').setErrors({incorrect: true});
+    }
+    if (form.get('lastName').value.trim() === '') {
+      form.get('lastName').setValue('');
+      form.get('lastName').setErrors({incorrect: true});
+    }
+    if (form.get('title').value === '') {
+      form.get('title').setValue('');
+      form.get('title').setErrors({incorrect: true});
+    }
+  }
+
+  memberFormHasErrors(form: FormGroup): boolean {
+    if (form.get('team').value === '') {
+      return true;
+    }
+    if (form.get('firstName').value.trim() === '') {
+      return true;
+    }
+    if (form.get('lastName').value.trim() === '') {
+      return true;
+    }
+    if (form.get('title').value === '') {
+      return true;
+    }
+    return false;
   }
 }
